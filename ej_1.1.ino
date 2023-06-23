d1d477806e756744109df82c15ca4504a032c8cb
bnxxx <tatiferreria@gmail.com>
2023-06-23 09:36:25 -0300

    arduino

---
+++ ej_1.1.ino
@@ -0,0 +1,26 @@
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2,1);
+  digitalWrite(3,1);
+  delay(125);
+  digitalWrite(3,0);
+  delay(125);
+  digitalWrite(3,1);
+  delay(125);
+  digitalWrite(3,0);
+  delay(125);
+  digitalWrite(3,1);
+  digitalWrite(2,0);
+  delay(125);
+  digitalWrite(3,0);
+  delay(125);
+  digitalWrite(3,1);
+  delay(125);
+  digitalWrite(3,0);
+  delay(125);
+  digitalWrite(3,1);
+}
